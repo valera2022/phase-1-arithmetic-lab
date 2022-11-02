8400061c2ee57c3548fd026ff0a63362db31fb4f
GeralmyUrena <geralmyuri11@gmail.com>
2022-11-02 18:35:27 -0400

    commit tree a5c0ea5fe7ae37e6535b27c7c3dbe08fdb8f8604

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+const multiply = num1 * num2 
+multiply 
+
+const random = Math.ceil (Math.random() * 10) 
+
+let num3 = 19
+let num4 = 5
+
+const mod = num3 % num4
+mod
+
+const max = Math.max (1,2,3,5,20)
